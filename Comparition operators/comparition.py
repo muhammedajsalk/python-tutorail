7d02e55b56a64e4ff2d91891ca34855ebf1d5b07
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-05-30 20:12:33 +0530

    Comparition operator

---
+++ Comparition operators/comparition.py
@@ -0,0 +1,44 @@
+# < strictly less than
+3<5
+#true
+5<2
+#false
+
+# <= less than or equal
+x=10
+y=10
+x<=y
+#true
+
+# > strictly greater than
+3>6
+#false
+6>1
+#true
+
+# >= greater than or equal
+6>=5
+#true
+
+# == equal
+x==y
+#true
+
+# != not equal
+x!=y
+#false
+
+"a" in "apple"
+#true
+"a" not in "apple"
+#false
+
+list1=["Hello","world"]
+string="Hello world"
+list2=string.split()
+list2
+#['Hello','World']
+list1 is list2
+#false
+
+
