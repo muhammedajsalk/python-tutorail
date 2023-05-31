4a8aed487270d8350903ed633833880292bb4e73
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-05-31 21:19:41 +0530

    Logical Operations with boolean

---
+++ Logical Operations with Boolean/logical.py
@@ -0,0 +1,9 @@
+10>5 and 5<20
+#True
+
+10>5 or 5>20
+#True
+
+to_be=True
+not to_be
+#False
---
+++ Object in python/object.py
@@ -0,0 +1,1 @@
+#in python,all values are stored in object. you can think that an object is like a box that contain information about some value and also stores some additional data such as its identity
