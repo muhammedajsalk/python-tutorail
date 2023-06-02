5608aef083a8b1b8e7fc12b133ab9218d54a0601
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-06-02 09:37:47 +0530

    slicing

--- Collection/index/index.py
+++ Collection/index/index.py
@@ -2,10 +2,14 @@ x=[1,2,3,4,5]
 p=[0,1,2,3,4]
 
 print(x[2])
-
+#3
 a="hello"
 print(a[1])
+#e
 
 print(x[-1])
+#5
 print(x[-2])
+#4
 print(x[-3])
+#3
--- Collection/list/list.py
+++ Collection/list/list.py
@@ -3,14 +3,20 @@ y=["a","b","c","d","e"]
 z=["Hello","world"]
 
 print(x)
+#[1,2,3,4,5]
 print(y)
+#["a","b","c","d","e"]
 print(z)
+#z=["Hello","world"]
 
 a=list("hello")
 print(a)
+#['h', 'e', 'l', 'l', 'o']
 
 b=list("500")
 print(b)
+#['5', '0', '0']
 
 c=[1,"b","hello",[1,2]]
 print(len(c))
+#4
