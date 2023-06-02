93b532193a744352bbaee8b4ce30539132979bf6
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-06-02 09:05:05 +0530

    collection folder added and list added

---
+++ Collection/list/list.py
@@ -0,0 +1,16 @@
+x=[1,2,3,4,5]
+y=["a","b","c","d","e"]
+z=["Hello","world"]
+
+print(x)
+print(y)
+print(z)
+
+a=list("hello")
+print(a)
+
+b=list("500")
+print(b)
+
+c=[1,"b","hello",[1,2]]
+print(len(c))
