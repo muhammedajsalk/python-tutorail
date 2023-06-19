b9638ae9632151b98b28c678853f50d8e8c04b86
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-06-19 10:34:11 +0530

    control flow statements added with simple if topic

---
+++ control flow statement/Simple if/hello.py
@@ -0,0 +1,42 @@
+hello=True
+if  hello == True:
+    print("HELLO AJSAL")
+
+#HELLO AJSAL
+
+
+hi=False
+if hi==True:
+    print("Hi AJSAL")
+#
+
+
+helloy=False
+if not helloy:
+    print("helloy Ajsal ")
+#helloy Ajsal
+
+
+x=10
+if x == 10:
+    print("x ajsal")
+#x ajsal
+
+
+x=10
+if x > 100:
+    print("x ajsal")
+#
+
+
+x=10
+if x < 100:
+    print("x ajsal")
+#x ajsal
+
+
+x=10
+if x < 10:
+    if x ==10:
+        print("hello two ajsal")
+#hello two ajsal
