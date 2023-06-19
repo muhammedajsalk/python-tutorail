9e598825344684a19633fa622e69bd26af2f88bf
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-06-19 07:47:49 +0530

    fuctions added fist topic introduction

---
+++ Factions/introduction/intro.py
@@ -0,0 +1,18 @@
+def hello():
+    print("hello world")
+
+hello()
+hello()
+hello()
+hello()
+
+#hello world
+#hello world
+#hello world
+#hello world
+
+def add(x,y):
+    return x+y
+
+z=add(10,20)
+print(z)
