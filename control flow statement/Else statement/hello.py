7284ab4e81f37beeeec6c8c5d8af49595a406ff6
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-06-19 10:46:59 +0530

    Else statement

---
+++ control flow statement/Else statement/hello.py
@@ -0,0 +1,34 @@
+x=101
+
+if x<100:
+    print("hello world")
+else:
+    print(x)
+#101
+
+
+x=10
+
+if x<100:
+    print("hello world")
+else:
+    print(x)
+#hello world
+
+
+x=101
+
+if x<100:
+    print("hello world")
+else:
+    pass
+#
+
+
+x=101
+
+if x<100:
+    pass
+else:
+    print(x)
+#101
