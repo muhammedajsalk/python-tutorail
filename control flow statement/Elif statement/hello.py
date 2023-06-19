481ebdea47de9cdf49b6bea9cb22d75e5a416954
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-06-19 10:58:49 +0530

    Elif statement

---
+++ control flow statement/Elif statement/hello.py
@@ -0,0 +1,28 @@
+price=1000
+if price > 900:
+    print("Too expensive")
+elif price > 500:
+    print("Affordable")
+else:
+    print("Cheap!!!")
+#Too expensive
+
+
+price=900
+if price > 900:
+    print("Too expensive")
+elif price > 500:
+    print("Affordable")
+else:
+    print("Cheap!!!")
+#affordable
+
+
+price=100
+if price > 900:
+    print("Too expensive")
+elif price > 500:
+    print("Affordable")
+else:
+    print("Cheap!!!")
+#cheap!!!
