e075aaef295402f8cbc0e3ff78f50368e6b2dbae
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-07-02 09:46:57 +0530

    Nested Loop

---
+++ control flow statement/Nasted Loop/hello.py
@@ -0,0 +1,18 @@
+names=["David","Ashley"]
+surnames=["miller"]
+for name in names:
+    for surname in surnames:
+        print(name,surname)
+#David miller
+#Ashley miller
+
+
+names=["David","Ashley"]
+surnames=["miller","joe"]
+for name in names:
+    for surname in surnames:
+        print(name,surname)
+#David miller
+#David joe
+#Ashley miller
+#Ashley joe
