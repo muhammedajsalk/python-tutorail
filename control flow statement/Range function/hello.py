138e64a8c4ea27bfbdc3571c15d56d28a0d1ac6b
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-07-02 09:35:21 +0530

    range function

---
+++ control flow statement/Range function/hello.py
@@ -0,0 +1,18 @@
+for number in range(5):
+    print(number)
+#0,1,2,3,4
+
+for number in range(1,5):
+    print(number)
+#1,2,3,4
+
+for number in range(1,10,2):
+    print(number)
+#1,3,5,7,9
+
+for _ in range(5):
+    print("Hello ajsal")
+#Hello ajsal,Hello ajsal,Hello ajsal,Hello ajsal,Hello ajsal
+
+
+
