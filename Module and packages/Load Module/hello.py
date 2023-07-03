cef43c1a90c272b4805499d151c43c404580a57b
muhammedajsalk <ajsalmuhammed747@gmail.com>
2023-07-03 12:00:30 +0530

    Load Module

---
+++ Module and packages/Load Module/hello.py
@@ -0,0 +1,14 @@
+#type of import
+
+#1st type
+import math
+print(math.pi)
+#3.141592653589793
+
+#2st type
+from math import sqrt
+print(sqrt(16))
+#4.0
+
+from random import choice
+print(choice([1,2,3,4,5,6,7,8,9,0]))
